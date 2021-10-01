4b816d3e828930578ff18b466fb0090f530852e1
vanahdren <troger43@uncc.edu>
2021-10-01 16:51:24 -0400

    .

--- itis3135/scripts/activity6.js
+++ itis3135/scripts/activity6.js
@@ -5,15 +5,17 @@ var x;
 
 
 while (i==0)
-    {  
+    {  console.log(polyType);
          if(validateEntry(polyType))
         { 
+            console.log(polyType);
         var type = polyFinder(polyType);
         alert (type);       
         i=1;
         }
         else
             {
+                console.log(polyType);
             polyType = "Please enter a Nonzero number between 1 and 10"
             }
     
