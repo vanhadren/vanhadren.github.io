f34aefc636865409b4ec1a57a8d671a644348873
vanahdren <troger43@uncc.edu>
2021-10-01 15:50:55 -0400

    .

--- itis3135/scripts/activity6.js
+++ itis3135/scripts/activity6.js
@@ -1,73 +1,70 @@
 
-var i =0;
-var x =0;
+var i = 0;
+var x = 0;
 
-function validateEntry(sides)
-{
-    if(typeof sides == "number")
-    {
-        if (sides >= -10 && sides <= 10)
-        {
+function validateEntry(sides) {
+    if (typeof sides == "number") {
+        if (sides >= -10 && sides <= 10) {
             return (true);
         }
     }
-    else
-    {
+    else {
         return false;
     }
 }
 
-function polyFinder(x)
-{
-//set variables with prompt
-var sides = prompt("welcome. enter number of sides");
-   while (!validateEntry(sides))
-{
-       sides = prompt("please enter a number between 1 and 10");
-       validateEntry(sides);
-} 
-//set validated num to x
-
-    x = Math.round(Math.abs(sides));
-    console.log(x);
-        switch(x)
-        {
-            case 0:
-                alert("entry can not be ZERO");
-                break;
+function polyFinder(x) {
+    //set variables with prompt
+    var sides = prompt("welcome. enter number of sides");
+    while (i = 0) {
+        if (validateEntry(sides)) {
+            x = Math.round(Math.abs(sides));
+            console.log(x);
+            switch (x) {
+                case 0:
+                    alert("entry can not be ZERO");
+                    break;
                 case 1:
-                alert("1 side = Henagon");
-                break;
+                    alert("1 side = Henagon");
+                    break;
                 case 2:
-                alert("2 side = Digon");
-                break;
+                    alert("2 side = Digon");
+                    break;
                 case 3:
-                alert("3 side = Trigon");
-                break;    
+                    alert("3 side = Trigon");
+                    break;
                 case 4:
-                alert( "4 side = Tetragon");
-                break;
+                    alert("4 side = Tetragon");
+                    break;
                 case 5:
-                alert( "5 side = Pentagon");
-                break;
+                    alert("5 side = Pentagon");
+                    break;
                 case 6:
-                alert( "6 side = Hexagon");
-                break;
+                    alert("6 side = Hexagon");
+                    break;
                 case 7:
-                alert( "7 side = Heptagon");
-                break;
+                    alert("7 side = Heptagon");
+                    break;
                 case 8:
-                alert( "8 side = Octagon");
-                break;
+                    alert("8 side = Octagon");
+                    break;
                 case 9:
-                alert( "9 side = Nonagon");
-                break;
+                    alert("9 side = Nonagon");
+                    break;
                 case 10:
-                alert( "10 side = Decagon");
-                break;
+                    alert("10 side = Decagon");
+                    break;
                 default:
-                alert( "please enter a NONZERO number between 1 and 10.");
-                break;
-                
+                    alert("please enter a NONZERO number between 1 and 10.");
+                    break;
+
+            }
         }
+        else {
+
+
+            sides = prompt("please enter a number between 1 and 10");
+
+        }
+    }
 }
