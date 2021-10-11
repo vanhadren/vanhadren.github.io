acf4215a78acbb9252655e0dad3b6d03c2956f8c
Vanhadren <troger43@uncc.edu>
2021-10-10 22:50:42 -0400

    .

--- itis3135/scripts/acitivity8.js
+++ itis3135/scripts/acitivity8.js
@@ -1,94 +1,84 @@
 var salaries = [];
 var people = [];
 
-var select = document.getElementById("peopleArr"); 
-var names = ["John Wick","Jason Bourne","John McClane","Chuck Norris","Jack Reacher","Anakin Skywalker"]; 
-
-for(var i = 0; i < names.length; i++) 
-{
-    var temp = names[i];
-    var element = document.createElement("OPTION");
-    element.textContent = temp;
-    element.value = temp;
-    select.appendChild(element);
-
-}
-/*
-var select = document.getElementById("peopleArr");
-var names = ["tom", "dick", "harry"];
-
-for (var i=0; i<names.length; i++)
-{
-    var option = document.createElement("OPTION");
-    var txt = document.createTextNode(names[i]);
-    
-    option.appendChild(txt);
-    select.insertBefore(option,select.lastChild);
-}
-
-*/
-
-window.onload = function() 
+window.onload = function () 
 {
     document.getElementById("DisplayResults").onclick = displayResults;
     document.getElementById("DisplaySalary").onclick = displaySalary;
     document.getElementById("AddSalary").onclick = addSalary;
     document.getElementById("peopleArr").focus();
-
-
 }
 
-
-function displayResults ()
+function populateDropDown ()
 {
-    let sum=0;
-    
+    var select = document.getElementById("peopleArr");
+    var names = ["John Wick", "Jason Bourne", "John McClane", "Chuck Norris", "Jack Reacher", "Anakin Skywalker"];
 
-    for(let i = 0; i < salaries.length; i++)
-    {
-         sum += salaries[i];
-    }
-
-    let average = sum/salaries.length;
-    let largest;
-
-    for(let j=0; j<salaries.length; j++)
-    {
-        let temp=0;
-        if(salaries[j] > temp)
+        for (var i = 0; i < names.length; i++) 
         {
-            largest = salaries[j];
+            var temp = names[i];
+            var element = document.createElement("OPTION");
+            element.textContent = temp;
+            element.value = temp;
+            select.appendChild(element);
+
         }
+/*
+var select = document.getElementById("peopleArr");
+var names = ["tom", "dick", "harry"];
+    for (var i=0; i<names.length; i++)
+    {
+        var option = document.createElement("OPTION");
+        var text = document.createTextNode(names[i]);      
+            option.appendChild(txt);
+            select.insertBefore(option, select.lastChild);
     }
-document.getElementById("results").innerHTML = "<h2> RESULTS <h2>" + "<p> Largest Salary = " + largest+ "</p>"+"<p> Average Salary = " + average;
+*/
+}
+function displayResults()
+ {
+    let sum = 0;
+        for (let i = 0; i < salaries.length; i++)
+        {
+            sum += salaries[i];
+        }
+            let average = sum / salaries.length;
+
+            let largest;
+
+                for (let j = 0; j < salaries.length; j++) 
+                {
+                    let temp = 0;
+                    if (salaries[j] > temp) 
+                    {
+                        largest = salaries[j];
+                    }
+        }
+    document.getElementById("results").innerHTML = "<h2> RESULTS <h2>" + "<p> Largest Salary = " + largest + "</p>" + "<p> Average Salary = " + average;
 }
 
-function displaySalary()
-{
+function displaySalary() {
     // get names and calues from arrays
     //display them as rows in the HTML table element with id="results_table" 
-    var display= ""
+    var display = ""
     display += "<tr><th> Salary </th> <th> People </th></tr>"
-        for(let i=0; i<people.length; i++)
-        {
-            display +="<tr><td>" +salaries[i]+ "</td><td>"+ people[i] +"</td></tr>";
-        }
-        display += "</table>";
-        document.getElementById("salaryTable").innerHTML = "<h2> SALARIES </h2>" + display;
+    for (let i = 0; i < people.length; i++) {
+        display += "<tr><td>" + salaries[i] + "</td><td>" + people[i] + "</td></tr>";
+    }
+    display += "</table>";
+    document.getElementById("salaryTable").innerHTML = "<h2> SALARIES </h2>" + display;
 }
 
-function addSalary()
-{   
+function addSalary() {
     var person = document.getElementById("peopleArr").value;
     var salary = document.getElementById("salary").value;
     //add person and salaray to the array
     //data valadation must not be empty and value must be numeric
-    if(salary =="" || salary == NaN)
-    {
+    if (salary == "" || salary == NaN) {
         alert("check your input");
         document.getElementById("name").focus();
     }
-    else{
+    else {
         salary = parseInt(salary);
         salaries.push(salary);
         people.push(person);
