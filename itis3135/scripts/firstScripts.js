b849d8fe5cf26826fd3e24cf4f347c52b3826b35
Vanhadren <troger43@uncc.edu>
2021-10-21 12:52:20 -0400

    .

--- itis3135/scripts/firstScripts.js
+++ itis3135/scripts/firstScripts.js
@@ -43,7 +43,7 @@
 */
         window.onload= function()
         {
-            timeNdate();
+          //  timeNdate();
             document.getElementById("sumButton").onclick=sumOfThree();
             document.getElementById("difButton").onclick=difOfThree();
         }
