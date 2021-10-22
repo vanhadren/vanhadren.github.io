c0cbe7abffb25eaa138fd108689db02612222983
Vanhadren <troger43@uncc.edu>
2021-10-22 11:36:04 -0400

    .

---
+++ itis3135/scripts/slideshow.js
@@ -0,0 +1,20 @@
+$(document).ready(function()
+{
+    $(".slide:first").show();
+        setInterval(function()
+            {
+                Next($('.slide:visible'))
+            }, 4000);
+});
+function Next(slide)
+{
+    slide.fadeOut();
+        if(typeof slide.next().attr('src') !== "undefinded")
+            {
+                slide.next().fadeIn();
+            }
+        else
+            {
+                $('.slide:first').fadeIn();
+            }
+}
