cffe61225836bfdc2677f2b125a90bc1f4bb954e
Vanhadren <troger43@uncc.edu>
2021-10-24 18:21:36 -0400

    .

--- itis3135/scripts/slideshow.js
+++ itis3135/scripts/slideshow.js
@@ -1,14 +1,11 @@
-window.onload = function()
-{
-function firstSlide(slide)
+$(document).ready(function()
 {
     $(".slide:first").show();
-        setInterval(
-            function()
+        setInterval(function()
             {
                 Next($('.slide:visible'))
             }, 4000);
-}
+});
 function Next(slide)
 {
     slide.fadeOut();
@@ -20,5 +17,4 @@ function Next(slide)
             {
                 $('.slide:first').fadeIn();
             }
-}
 }
