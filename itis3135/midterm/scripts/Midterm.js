d50e2f01994d7946464cd555efd0ebcc9e75e4cf
Vanhadren <troger43@uncc.edu>
2021-10-29 22:56:19 -0400

    .

--- itis3135/midterm/scripts/Midterm.js
+++ itis3135/midterm/scripts/Midterm.js
@@ -3,14 +3,14 @@
 //find average calories
 // find maximum calories
 //cheat day
+window.ready = function() {
+    //event handlers
 
 var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 var calories = [3000,2500,1500,4000,2200,1200,4400];
 
 var $ = function(id) { return document.getElementById(id); };
 
-window.onload = function() {
-    //event handlers
 
 
 var numCalories = $("calories");
