498481dab3390d9c03b8474e9c16b50d705a71c2
Vanhadren <troger43@uncc.edu>
2021-11-05 11:25:09 -0400

    activity 10

---
+++ itis3135/scripts/animatedslideshow.js
@@ -0,0 +1,26 @@
+
+ $(document).ready(function()
+{
+    $("images_list a").each(function()
+    {
+        var imageURL = $this.attr("href");
+        var caption = $this.attr("title");
+
+        var galleryImage = new Image();
+        galleryImage.src = imageURL;
+
+        $(this).click(function(evt) 
+        {
+            $("#caption,#image").slideUp(2000,
+                function()
+                {
+                    $("#image").attr("src",imageURL);
+                    $("#caption").text(caption);
+                    $("#caption,#image").slideDown(2000);
+
+                });
+                evt.preventDefault();
+        });
+    });
+    $("li:first-child a").focus();
+});
