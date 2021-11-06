99f004cf43c81d48da8b3cfacf71aad77600b487
vanhadren <troger43@uncc.edu>
2021-11-06 16:07:08 -0400

    .

--- itis3135/rojo/js/slideshow.js
+++ itis3135/rojo/js/slideshow.js
@@ -35,7 +35,7 @@ window.onload =function()
                         {                            
                             $(divID.lastElementChild).hide();
                             temp=0;
-                            $(tempIMG).show();
+                            $(divID.firstElementChild).show();
                         }                        
                     
             },2000);
