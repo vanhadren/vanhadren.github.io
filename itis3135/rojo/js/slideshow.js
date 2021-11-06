181c7aa8011c5b1a2f454e79158fe530ba7dc203
vanhadren <troger43@uncc.edu>
2021-11-06 15:48:22 -0400

    fixed js

--- itis3135/rojo/js/slideshow.js
+++ itis3135/rojo/js/slideshow.js
@@ -32,7 +32,7 @@ window.onload =function()
                         temp++;
                         if(temp>images.length)
                         {                            
-                            $(divID.lastElementChild).hide();
+                            $(divID.lastElementChild().hide());
                             temp=0;
                         }                        
                     
