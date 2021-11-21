204a3814267022c9bd075739add130acb9534b40
vanhadren <troger43@uncc.edu>
2021-11-20 20:56:02 -0500

    added activity 13 to site

---
+++ itis3135/scripts/rogers-Activity13.js
@@ -0,0 +1,30 @@
+$(document).ready(function()
+ {
+    $.ajax
+    ({
+        type: "get",
+        url: "compnents/team.json",
+        beforeSend: function() {
+            $("#team").html("Loading...");
+        },
+        timeout: 10000,
+        error: function(xhr, status, error) {
+            alert("Error: " + xhr.status + " - " + error);
+        },
+        dataType: "json",
+        success: function(data) 
+        {
+            $("#team").html("");
+            $.each(data, function()
+            {
+                $.each(this, function(temp, tempVal)                
+                    {              
+                        $("#team").append("NAME" + "<br>" + "&emsp;"+ tempVal.name + "<br>" +
+                                        "TITLE" + "<br>" + "&emsp;"+  tempVal.title + "<br>" +
+                                        "BIO" + "<br>" +  tempVal.bio + "<br><br>"
+                                        );            
+                    });
+            }       );
+        }
+    });
+});
