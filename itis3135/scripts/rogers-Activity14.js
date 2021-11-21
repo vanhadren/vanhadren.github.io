b358c6820d5d1d519172fb1853316582f6be2047
vanhadren <troger43@uncc.edu>
2021-11-20 23:03:26 -0500

    .

--- itis3135/scripts/rogers-Activity14.js
+++ itis3135/scripts/rogers-Activity14.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
         let title = $(this).children("a").attr("title");
         $.get(title + ".json", function(data, status){
             data = data['speakers'][0];
-            $("main h1").html(data['tile']);
+            $("main h1").html(data['title']);
             $("main h2").html(data['month']+"<br/>"+ data['speaker']);
             $("main img").attr("src", data.image);
             $("main p").html(data.text);
