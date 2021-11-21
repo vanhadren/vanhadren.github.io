5bd197a557138b66bcf610575240dd17cc355d61
vanhadren <troger43@uncc.edu>
2021-11-20 22:56:33 -0500

    .

--- itis3135/scripts/rogers-Activity14.js
+++ itis3135/scripts/rogers-Activity14.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-	
+	$("#nav_list li").click(function() {
+        let title = $(this).children("a").attr("title");
+        $.get(title + ".json", function(data, status){
+            data = data['speakers'][0];
+            $("main h1").html(data['tile']);
+            $("main h2").html(data['month']+"<br/>"+ data['speaker']);
+            $("main img").attr("src", data.image);
+            $("main p").html(data.text);
+        });
+    });
 }); // end ready
