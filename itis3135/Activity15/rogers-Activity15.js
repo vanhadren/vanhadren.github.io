8bf741456dc132f50239a5a63b49eb2606ab6d63
Vanhadren <troger43@uncc.edu>
2021-11-29 20:02:33 -0500

    .

---
+++ itis3135/Activity15/rogers-Activity15.js
@@ -0,0 +1,19 @@
+$(document).ready(function()
+{
+    $.getJSON("team.json", function(data)
+    {
+        $.each(data, function()
+        {
+            $.each(this,function(key,value)
+            {
+                $("#team").append("<img src ="+"'"+value.image+"'><br>"
+                                    +"<h2>"+value.full_name+"</h2>"
+                                    +"<h3>"+value.department+"</h3>"
+                                    +"<p>"+value.bio+"</p>"
+                                    );
+            
+            });
+        });
+    });
+
+});
